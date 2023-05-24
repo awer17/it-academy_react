f5879ca674c23cfead5ec29c57c6796044b78460
Siarhei Siarheyeu <95449526+awer17@users.noreply.github.com>
2023-05-23 21:05:06 -0600

    feat: shop

---
+++ homework_2/shop/ItemShop.js
@@ -0,0 +1,32 @@
+var ItemShop = React.createClass({
+    displayName: 'ItemShop',
+
+    // propTypes: {
+    //     key: eact.PropTypes.number,
+    //     brand: React.PropTypes.string,
+    //     madel: React.PropTypes.string,
+    //     year: React.PropTypes.string,
+    //     foto: React.PropTypes.string,
+    //     description: React.PropTypes.string,
+    //     priсe: React.PropTypes.string,
+    //     control: React.PropTypes.string
+    // },
+
+    render: function(){
+        return <tr>
+            <td>+1+</td>
+            <td>+1+</td>
+            </tr>
+        //  React.DOM.tr( {className: '_item-car'}, 
+        //         React.DOM.td(null, this.props.brand),
+        //         React.DOM.td(null, this.props.madel),
+        //         React.DOM.td(null, this.props.year),
+        //         React.DOM.td(null,
+        //             React.DOM.img({src: this.props.foto, alt: this.props.brand + " "+ this.props.madel})),
+        //         React.DOM.td(null, this.props.description),
+        //         React.DOM.td(null, this.props.priсe),
+        //         React.DOM.td(null, 
+        //             React.DOM.button(null, this.props.control)),
+        //     )
+    },
+})
--- homework_2/shop/script.js
+++ homework_2/shop/script.js
@@ -3,17 +3,12 @@ var MyComponent = React.createClass({
     displayName: 'MyComponent',
     
     propTypes: {
-        cbFreeAnswerTextChanged: React.PropTypes.func.isRequired,
-    },
-
-    свfreeAnswerTextChanged: function(EO) { 
-        console.log('значение - '+EO.target.value); 
-        this.props.cbFreeAnswerTextChanged(EO.target.value);
+        car:React.PropTypes.array,
+        title:React.PropTypes.string,
     },
 
     render: function(){
 
-        
         var columName = []
         var arrColum = Object.keys(this.props.car[0])
         for ( var i = 0; i < arrColum.length; i++ ){
@@ -21,28 +16,19 @@ var MyComponent = React.createClass({
             if( columNameItem !== "code"){
                 columName.push(React.DOM.th({key:i, className:"_columName"}, columNameItem))
             }
-
         }
-        
-        // var columName =  Object.keys(this.props.car[0]).map( (elem, index) =>
-        //     React.DOM.th({key:index, className:"_columName"},elem)
+
+        // var itemElem = this.props.car.map( elem =>
+        //     React.createElement( ItemShop, {key: elem.code,
+        //         brand: elem.brand,
+        //         madel: elem.madel,
+        //         year: elem.year,
+        //         foto: elem.foto,
+        //         description: elem.description,
+        //         priсe: elem.priсe,
+        //         control: elem.control 
+        //     })
         // )
-
-        var answerCode = this.props.car.map( elem =>
-            React.DOM.tr( {key:elem.code, className: '_item-car'}, 
-                React.DOM.td(null, elem.brand),
-                React.DOM.td(null, elem.madel),
-                React.DOM.td(null, elem.year),
-                React.DOM.td(null,
-                    React.DOM.img({src: elem.foto, alt: elem.brand + " "+ elem.madel})),
-                React.DOM.td(null, elem.description),
-                React.DOM.td(null, elem.priсe),
-                React.DOM.td(null, 
-                    React.DOM.button(null, elem.control)),
-            ))
-
-
-
 
         return React.DOM.div(
             {className: 'MyComponent'},
@@ -50,9 +36,8 @@ var MyComponent = React.createClass({
             React.DOM.table( null,
                 React.DOM.thead( null,
                     React.DOM.tr(null, columName)),
-                React.DOM.tbody(null, answerCode)
+                React.DOM.tbody(null, ItemShop)
                 )
         );
     },
 });
-//370
