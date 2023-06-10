4f8a03c5025c035eb4b790ac8b93a524ef52ebfd
Siarhei Siarheyeu <95449526+awer17@users.noreply.github.com>
2023-06-10 17:45:10 -0600

    feat: homework 4 Br2jsx

---
+++ homework_4 br2jsx/App.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Br2jsx from './components/Br2jsx';
+
+
+
+
+ReactDOM.render(
+  <Br2jsx
+  
+  />,
+  document.getElementById('container') 
+);
+
---
+++ homework_4 br2jsx/components/Br2jsx.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { render } from 'react-dom';
+import './Br2jsx.css';
+
+class Br2jsx extends React.Component{
+    static propTypes = {
+    }
+
+    render(){
+
+        return <div className='wrap'>2</div>
+    }
+}
+
+
+export default Br2jsx;
