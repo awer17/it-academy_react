6a091fa2288ac2f3bc1ea38283a647e0adb85e31
Siarhei Siarheyeu <95449526+awer17@users.noreply.github.com>
2023-06-10 18:16:30 -0600

    feat: Br2jsx

--- homework_4 br2jsx/App.js
+++ homework_4 br2jsx/App.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 
 import Br2jsx from './components/Br2jsx';
 
+let text="первый<br>второй<br/>третий<br />последний";
 
 
 
 ReactDOM.render(
   <Br2jsx
-  
+    text ={text}
   />,
   document.getElementById('container') 
 );
--- homework_4 br2jsx/components/Br2jsx.js
+++ homework_4 br2jsx/components/Br2jsx.js
@@ -5,11 +5,18 @@ import './Br2jsx.css';
 
 class Br2jsx extends React.Component{
     static propTypes = {
+        text: PropTypes.string,
     }
 
     render(){
-
-        return <div className='wrap'>2</div>
+        const lines = this.props.text.split(/<br *\/?>/g);
+        const arr = [];
+        for(let i = 0; i < lines.length; i++){
+          if(i)
+            arr.push(<br/>)
+          arr.push(lines[i])
+        }
+        return <div className='wrap'>{arr}</div>
     }
 }
 
